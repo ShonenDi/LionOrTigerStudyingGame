c9a10daefb08856883ab8a166af70934a44e9fa2
Dima Usatyuk <Shonen1988@gmail.com>
2020-03-22 13:21:08 +0200

    added onClick method in img views and enum inner class

--- app/src/main/java/com/shonen/ukr/lionortiger/MainActivity.java
+++ app/src/main/java/com/shonen/ukr/lionortiger/MainActivity.java
@@ -3,12 +3,26 @@ package com.shonen.ukr.lionortiger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+        enum Player {
+           ONE, TWO;
+        }
+        Player currentPlayer = Player.ONE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void getAnimationOnImgView(View imgView) {
+        ImageView img = (ImageView) imgView;
+        img.setTranslationY(-2000);
+        img.setImageResource(R.drawable.tiger);
+        img.animate().translationYBy(2000f).rotation(3600).setDuration(3000);
+    }
+
 }
