4c0f968c00f314516ccdd07bc1c7306ad3b8bf38
Dima Usatyuk <Shonen1988@gmail.com>
2020-03-22 14:22:12 +0200

    add Arr of Player data type

--- app/src/main/java/com/shonen/ukr/lionortiger/MainActivity.java
+++ app/src/main/java/com/shonen/ukr/lionortiger/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-        enum Player {
-           ONE, TWO;
-        }
-        Player currentPlayer = Player.ONE;
+    enum Player {
+        ONE, TWO
+    }
+
+    Player currentPlayer = Player.ONE;
+    Player[] playersChoices = new Player[9];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,17 @@ public class MainActivity extends AppCompatActivity {
     public void getAnimationOnImgView(View imgView) {
         ImageView img = (ImageView) imgView;
         img.setTranslationY(-2000);
-        img.setImageResource(R.drawable.tiger);
-        img.animate().translationYBy(2000f).rotation(3600).setDuration(3000);
-    }
+        Toast playerOneTurn = Toast.makeText(getApplicationContext(), "Player one turn", Toast.LENGTH_SHORT);
+        playerOneTurn.show();
+        if (currentPlayer == Player.ONE) {
+            img.setImageResource(R.drawable.lion);
+            currentPlayer = Player.TWO;
+        } else if (currentPlayer == Player.TWO) {
+            img.setImageResource(R.drawable.tiger);
+            currentPlayer = Player.ONE;
+        }
+        img.animate().translationYBy(2000f).alpha(1).rotation(3600).setDuration(3000);
+        Toast.makeText(this, img.getTag().toString(), Toast.LENGTH_SHORT).show();
 
+    }
 }
