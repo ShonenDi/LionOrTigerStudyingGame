975747d8ee6aa59f0899849d71ec4481943223a6
Dima Usatyuk <Shonen1988@gmail.com>
2020-03-23 16:53:52 +0200

    add loop to find win condition

--- app/src/main/java/com/shonen/ukr/lionortiger/MainActivity.java
+++ app/src/main/java/com/shonen/ukr/lionortiger/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     Player currentPlayer = Player.ONE;
     Player[] playersChoices = new Player[9];
+    int[][] winnersRowNColumn = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    private boolean gameOver = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +26,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void getAnimationOnImgView(View imgView) {
         ImageView img = (ImageView) imgView;
-        img.setTranslationY(-2000);
-        Toast playerOneTurn = Toast.makeText(getApplicationContext(), "Player one turn", Toast.LENGTH_SHORT);
-        playerOneTurn.show();
-        if (currentPlayer == Player.ONE) {
-            img.setImageResource(R.drawable.lion);
-            currentPlayer = Player.TWO;
-        } else if (currentPlayer == Player.TWO) {
-            img.setImageResource(R.drawable.tiger);
-            currentPlayer = Player.ONE;
-        }
-        img.animate().translationYBy(2000f).alpha(1).rotation(3600).setDuration(3000);
-        Toast.makeText(this, img.getTag().toString(), Toast.LENGTH_SHORT).show();
+        int index = Integer.parseInt(img.getTag().toString());
+        if (playersChoices[index] == null && gameOver == false) {
+            playersChoices[index] = currentPlayer;
+            img.setTranslationY(-2000);
+            Toast playerOneTurn = Toast.makeText(getApplicationContext(), "Player one turn", Toast.LENGTH_SHORT);
+            playerOneTurn.show();
+            if (currentPlayer == Player.ONE) {
+                img.setImageResource(R.drawable.lion);
+                currentPlayer = Player.TWO;
+            } else if (currentPlayer == Player.TWO) {
+                img.setImageResource(R.drawable.tiger);
+                currentPlayer = Player.ONE;
+            }
+            img.animate().translationYBy(2000f).alpha(1).rotation(3600).setDuration(3000);
+            Toast.makeText(this, img.getTag().toString(), Toast.LENGTH_SHORT).show();
 
+            for (int[] winColumn : winnersRowNColumn) {
+                if (playersChoices[winColumn[0]] == playersChoices[winColumn[1]] &&
+                        playersChoices[winColumn[1]] == playersChoices[winColumn[2]] && playersChoices[winColumn[0]] != null) {
+                    gameOver = true;
+                    String winner = "";
+                    if (currentPlayer == Player.ONE) {
+                        winner = "Player two ";
+                    } else if (currentPlayer == Player.TWO) {
+                        winner = "Player one ";
+                    }
+                    Toast.makeText(this, winner + "Win the game", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }
     }
 }
